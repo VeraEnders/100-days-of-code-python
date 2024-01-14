d184ad54b16ff831aff58b5b9bd46c5e3f257ca8
VeraEnders <veraabramo@gmail.com>
2024-01-14 20:52:34 +0100

    add tasks for day 6

---
+++ Day 1-14 Beginner Python/day-06-functions/06-00_exercise_functions-and-while.py
@@ -0,0 +1,11 @@
+# define the function
+def my_function():
+  print('Hello')
+
+# call the function
+my_function()
+
+step = 0
+while step < 10:
+  print(f"This is a step number: {step}")
+  step += 1
