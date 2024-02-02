0d5a29232bfba8f13478ca8bdd27b3ccb78ce1b1
VeraEnders <veraabramo@gmail.com>
2024-02-02 15:33:19 +0100

    add score class

--- Day 15-31 Intermediate/project_snake-game/main.py
+++ Day 15-31 Intermediate/project_snake-game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Score
 import time
 
 screen = Screen()
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+score = Score()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -28,5 +30,11 @@ while game_is_on:
   # Detect collision with food
   if snake.head.distance(food) < 20:
     food.refresh()
+    score.increase_score()
+
+  # Detect collision with wall
+  if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    game_is_on = False
+    score.game_over()
 
 screen.exitonclick()
