73dde37026890a27400c4400fadfa124f3bc9bf2
VeraEnders <veraabramo@gmail.com>
2024-02-03 16:39:19 +0100

    update score class and main functionality

--- Day 15-31 Intermediate/project_pong-game/main.py
+++ Day 15-31 Intermediate/project_pong-game/main.py
@@ -1,6 +1,8 @@
 from turtle import Screen
 from score import Score
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -11,6 +13,7 @@ screen.tracer(0)
 score = Score()
 right_paddle = Paddle((350, 0))
 left_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(left_paddle.go_up, "w")
@@ -21,6 +24,30 @@ screen.onkey(right_paddle.go_down, "Down")
 game_is_on = True
 
 while game_is_on:
+  time.sleep(ball.move_speed)
   screen.update()
+  ball.move()
+
+  # Detect collision with wall
+  if ball.ycor() > 280 or ball.ycor() < -280:
+    ball.bounce_y()
+
+  # Detect collision with right_paddle
+  if ball.distance(right_paddle) < 50 and ball.xcor() > 320: 
+    ball.bounce_x()
+   
+  # Detect collision with left_paddle
+  if ball.distance(left_paddle) < 50 and ball.xcor() < -320: 
+    ball.bounce_x()
+
+  # Detect right_paddle misses
+  if ball.xcor() > 380:
+    ball.reset_position()
+    score.increase_l_score()
+
+  # Detect left_paddle misses
+  if ball.xcor() < -380:
+    ball.reset_position()
+    score.increase_r_score()
 
 screen.exitonclick()
--- Day 15-31 Intermediate/project_pong-game/score.py
+++ Day 15-31 Intermediate/project_pong-game/score.py
@@ -1,18 +1,28 @@
 from turtle import Turtle
 
-
 ALIGNMENT = "center"
-FONT = ('Arial', 20, 'normal')
+FONT = ('Courier', 40, 'bold')
 
 class Score(Turtle):
+
   def __init__(self):
     super().__init__()
-    self.score = 0
+    self.l_score = 0
+    self.r_score = 0
     self.color("white")
     self.hideturtle()
-    self.print_score()
-   
-  def print_score(self):
     self.penup()
-    self.goto(0, 270)
-    self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+    self.update_score()
+
+  def update_score(self):
+    self.clear()
+    self.goto(0, 230)
+    self.write(f"{self.l_score} : {self.r_score}", align=ALIGNMENT, font=FONT)
+   
+  def increase_l_score(self):
+    self.l_score +=1
+    self.update_score()
+
+  def increase_r_score(self):
+    self.r_score +=1
+    self.update_score()
