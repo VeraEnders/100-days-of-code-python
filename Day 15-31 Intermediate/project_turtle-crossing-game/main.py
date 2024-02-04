5ae1eec7471e042f62d25d3bc759b8e1f48dcd6d
VeraEnders <veraabramo@gmail.com>
2024-02-04 23:51:07 +0100

    add player class

---
+++ Day 15-31 Intermediate/project_turtle-crossing-game/main.py
@@ -0,0 +1,20 @@
+import time
+from turtle import Screen
+from player import Player
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("Turtle Crossing Game")
+screen.tracer(0)
+
+player = Player()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+game_is_on = True
+while game_is_on:
+  time.sleep(0.1)
+  screen.update()
+
+screen.exitonclick()
