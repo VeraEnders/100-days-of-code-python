c805b2b5c1ea42721754841b8896e07e45e3ffa7
VeraEnders <veraabramo@gmail.com>
2024-02-06 13:07:43 +0100

    update snake-game project

--- Day 15-31 Intermediate/project_snake-game/main.py
+++ Day 15-31 Intermediate/project_snake-game/main.py
@@ -35,15 +35,15 @@ while game_is_on:
 
   # Detect collision with wall
   if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-    game_is_on = False
-    score.game_over()
+    snake.reset()
+    score.reset()
 
   # Detect collision with tail
   for segment in snake.segments:
     if segment == snake.head:
       pass
     elif snake.head.distance(segment) < 10:
-      game_is_on = False
-      score.game_over()
+      snake.reset()
+      score.reset()
 
 screen.exitonclick()
--- Day 15-31 Intermediate/project_snake-game/scoreboard.py
+++ Day 15-31 Intermediate/project_snake-game/scoreboard.py
@@ -1,27 +1,34 @@
 from turtle import Turtle
 
 ALIGNMENT = "center"
-FONT = ('Arial', 20, 'normal')
+FONT = ('Courier', 20, 'normal')
 
 class Score(Turtle):
   def __init__(self):
     super().__init__()
     self.score = 0
+    self.high_score = 0
     self.color("white")
     self.hideturtle()
-    self.print_score()
-   
-  def print_score(self):
     self.penup()
     self.goto(0, 270)
-    self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+    self.print_score()
+   
+  def print_score(self):    
+    self.clear()
+    self.write(f"Score: {self.score}. High score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
   def increase_score(self):
     self.score += 1
-    self.clear()
     self.print_score()
 
-  def game_over(self):
-    self.clear()
-    self.goto(0, 0)
-    self.write(f"GAME OVER.\nTotal score: {self.score}.", align=ALIGNMENT, font=FONT)
+  def reset(self):
+    if self.score > self.high_score:
+      self.high_score = self.score
+    self.score = 0
+    self.print_score()
+
+  # def game_over(self):
+  #   self.clear()
+  #   self.goto(0, 0)
+  #   self.write(f"GAME OVER.\nTotal score: {self.score}.", align=ALIGNMENT, font=FONT)
