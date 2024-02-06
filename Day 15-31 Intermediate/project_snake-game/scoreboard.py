4892eebeb19acbc26c9b2759e0a1bdb103a5e8aa
VeraEnders <veraabramo@gmail.com>
2024-02-06 13:48:43 +0100

    update high_score in snake game

--- Day 15-31 Intermediate/project_snake-game/scoreboard.py
+++ Day 15-31 Intermediate/project_snake-game/scoreboard.py
@@ -7,14 +7,19 @@ class Score(Turtle):
   def __init__(self):
     super().__init__()
     self.score = 0
-    self.high_score = 0
+    self.high_score = self.get_high_score()
     self.color("white")
     self.hideturtle()
     self.penup()
     self.goto(0, 270)
     self.print_score()
+
+  def get_high_score(self):
+    with open("data.txt") as file:
+      high_score = file.read()
+    return int(high_score)
    
-  def print_score(self):    
+  def print_score(self):
     self.clear()
     self.write(f"Score: {self.score}. High score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
@@ -24,7 +29,10 @@ class Score(Turtle):
 
   def reset(self):
     if self.score > self.high_score:
-      self.high_score = self.score
+      # Set new high score
+      with open("data.txt", mode="w") as file:
+        file.write(f"{self.score}")
+      self.high_score = self.get_high_score()
     self.score = 0
     self.print_score()
 
