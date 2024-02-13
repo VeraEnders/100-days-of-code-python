c4a80647d6745942fda20ed74d5344a88f414cd5
VeraEnders <veraabramo@gmail.com>
2024-02-13 14:18:58 +0100

    add project solution for day 29

--- Day 15-31 Intermediate/project_password-manager-app/main.py
+++ Day 15-31 Intermediate/project_password-manager-app/main.py
@@ -1,8 +1,43 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip 
+
+LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+  entry_password.delete(0, END)
+
+  random_letters = [random.choice(LETTERS) for _ in range(random.randint(8, 10))]
+  random_symbols = [random.choice(SYMBOLS) for _ in range(random.randint(2, 4))]
+  random_numbers = [random.choice(NUMBERS) for _ in range(random.randint(2, 4))]
+  
+  password_list = random_letters + random_symbols + random_numbers
+  random.shuffle(password_list)
+  password = "".join(password_list)
+
+  entry_password.insert(0, string=password)
+  pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+  website = entry_website.get()
+  email = entry_email.get()
+  password = entry_password.get()
+
+  if len(website) == 0 or len(password) == 0:
+    messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+  else:
+    is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
+
+    if is_ok:
+      with open("data.txt", "a") as file:
+        file.write(f"{website} | {email} | {password} \n")
+      entry_website.delete(0, "end")
+      entry_password.delete(0, "end")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -38,10 +73,10 @@ entry_password = Entry(width=32)
 entry_password.grid(column=1, row=3)
 
 # Buttons
-button_password = Button(text="Generate Password")
+button_password = Button(text="Generate Password", command=generate_password)
 button_password.grid(column=2, row=3)
 
-button_add = Button(text="Add", width=43)
+button_add = Button(text="Add", width=43, command=save)
 button_add.grid(column=1, row=4, columnspan=3)
 
 window.mainloop()
