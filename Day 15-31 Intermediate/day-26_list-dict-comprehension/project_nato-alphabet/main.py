ee5f524db93d9bcc4380e652c8b4c9c44488db6c
VeraEnders <veraabramo@gmail.com>
2024-02-14 16:01:28 +0100

    add exception handling to validate input

--- Day 15-31 Intermediate/day-26_list-dict-comprehension/project_nato-alphabet/main.py
+++ Day 15-31 Intermediate/day-26_list-dict-comprehension/project_nato-alphabet/main.py
@@ -7,7 +7,15 @@ dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
 # Create a list of the phonetic code words
 # from a word that the user inputs.
-word = input("Enter a word: ")
 
-result = [dict[letter.upper()] for letter in word]
-print(result)
+def generate_phonetic():
+  word = input("Enter a word: ")
+  try:
+    result = [dict[letter.upper()] for letter in word]
+  except KeyError:
+    print("Sorry, only letters in the alphabet please.")
+    generate_phonetic()
+  else:
+    print(result)
+
+generate_phonetic()
