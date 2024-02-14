aec24516b8dff0e9224b4b637bf920c28e823601
VeraEnders <veraabramo@gmail.com>
2024-02-14 16:40:49 +0100

    update save function to save data in json format

--- Day 15-31 Intermediate/project_password-manager-app/main.py
+++ Day 15-31 Intermediate/project_password-manager-app/main.py
@@ -1,7 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
 import random
-import pyperclip 
+import pyperclip
+import json
 
 LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -28,14 +29,32 @@ def save():
   email = entry_email.get()
   password = entry_password.get()
 
+  new_data = {
+    website: {
+      "email": email,
+      "password": password,
+    }
+  }
+
   if len(website) == 0 or len(password) == 0:
     messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
   else:
-    is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
+    try:
+      with open("data.json", "r") as data_file:
+        # Reading existing data
+        data = json.load(data_file)
+    except FileNotFoundError:
+      # Creating new file
+      with open("data.json", "w") as data_file:
+        json.dump(new_data, data_file, indent=2)
+    else:
+      # Updating data with new data
+      data.update(new_data)
 
-    if is_ok:
-      with open("data.txt", "a") as file:
-        file.write(f"{website} | {email} | {password} \n")
+      with open("data.json", "w") as data_file:
+        # Saving updated data
+        json.dump(data, data_file, indent=2)
+    finally:
       entry_website.delete(0, "end")
       entry_password.delete(0, "end")
 
